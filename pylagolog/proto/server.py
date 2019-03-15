68d8fba4ac3df1360c2f8305bb2ef13088d8488d
hibitomo <hibi.tomoya@lagopus.org>
2019-03-15 02:52:21 +0000

    Add option and log

--- pylagolog/proto/server.py
+++ pylagolog/proto/server.py
@@ -1,5 +1,9 @@
+import sys
 import grpc
 from concurrent import futures
+
+from oslo_config import cfg
+from oslo_log import log as logging
 
 import time
 from pylagolog.proto import pylagolog_pb2
@@ -9,6 +13,20 @@ from pylagolog.congress.policy_engines import agnostic
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+LOG = logging.getLogger(__name__)
+CONF = cfg.CONF
+DOMAIN = "lagolog-server"
+
+logging.register_options(CONF)
+logging.setup(CONF, DOMAIN)
+
+common_opts = [
+    cfg.StrOpt('port',
+        short='p',
+        default='[::]:10484',
+        help='An insecure port for accepting RPCs'),
+]
+
 class DatalogServicer(pylagolog_pb2_grpc.DatalogServicer):
 
     def __init__(self, runtime):
@@ -16,21 +34,27 @@ class DatalogServicer(pylagolog_pb2_grpc.DatalogServicer):
         return
 
     def ModRules(self, request_iterator, context):
-        print("ModRules")
+        LOG.info("ModRules:")
         for command in request_iterator:
+            LOG.info("%s: %s" % ("ADD" if command.Type == pylagolog_pb2.ADD else "DEL", command.Rule))
             self.run.insert(command.Rule)
         return pylagolog_pb2.Result(Result = pylagolog_pb2.SUCCESS)
 
     def Queries(self, request_iterator, context):
-        print("Queries")
         results = []
         for query in request_iterator:
+            LOG.info("Query: %s" % query.Query)
             ans = self.run.select(query.Query)
-            results.append(pylagolog_pb2.QueryResult(Result=ans))
+            if ans != "":
+                results.append(pylagolog_pb2.QueryResult(Result=ans))
+        LOG.info("ANS: %s" % results)
         for result in results:
             yield result
     
 def serve():
+    conf = cfg.ConfigOpts()
+    conf.register_cli_opts(common_opts)
+    conf(sys.argv[1:])
     policy = "default"
     run = agnostic.Runtime()
     run.create_policy(policy)
@@ -38,8 +62,8 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pylagolog_pb2_grpc.add_DatalogServicer_to_server(
         DatalogServicer(run), server)
-    server.add_insecure_port('[::]:10484')
-    print("Server Start")
+    server.add_insecure_port(conf.port)
+    LOG.info("Server Start %s" % (conf.port))
     server.start()
     try:
         while True:
